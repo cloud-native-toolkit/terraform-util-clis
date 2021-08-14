398e9d14fe7018c7c11c7a135f9e97d33a890dc4
Sean Sundberg <seansund@us.ibm.com>
2021-08-14 17:59:43 -0500

    Fixes output references
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,5 @@
 output "bin_dir" {
   description = "Directory where the clis were downloaded"
   value       = local.bin_dir
-  depends_on  = [
-    null_resource.setup-jq,
-    null_resource.setup-igc,
-    null_resource.setup-yq3,
-    null_resource.setup-yq4
-  ]
+  depends_on  = [null_resource.setup-binaries]
 }

c2f5ac5f7aa6f8035feef3fbe57b160b2dcd4c82
Sean Sundberg <seansund@us.ibm.com>
2021-08-14 17:58:27 -0500

    Condense to one null_resource
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,28 +3,12 @@ locals {
   bin_dir = var.bin_dir != "" ? var.bin_dir : "${path.cwd}/bin"
 }
 
-resource null_resource setup-jq {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-jq.sh '${local.bin_dir}'"
+resource null_resource setup-binaries {
+  triggers = {
+    always_run = timestamp()
   }
-}
-
-resource null_resource setup-igc {
-  depends_on = [null_resource.setup-jq]
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-igc.sh '${local.bin_dir}'"
-  }
-}
-
-resource null_resource setup-yq3 {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-yq3.sh '${local.bin_dir}'"
-  }
-}
-
-resource null_resource setup-yq4 {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-yq4.sh '${local.bin_dir}'"
+    command = "${path.module}/scripts/setup-binaries.sh '${local.bin_dir}'"
   }
 }

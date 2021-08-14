484fa662bbd3c64c3b0bb7e8d172c5ec28a8c4df
Sean Sundberg <seansund@us.ibm.com>
2021-08-14 18:00:22 -0500

    Fails on error
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/setup-binaries.sh
+++ scripts/setup-binaries.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 
 DEST_DIR="$1"
 
-"${SCRIPT_DIR}/setup-jq.sh" "${DEST_DIR}"
-"${SCRIPT_DIR}/setup-igc.sh" "${DEST_DIR}"
-"${SCRIPT_DIR}/setup-yq3.sh" "${DEST_DIR}"
-"${SCRIPT_DIR}/setup-yq4.sh" "${DEST_DIR}"
+"${SCRIPT_DIR}/setup-jq.sh" "${DEST_DIR}" || exit 1
+"${SCRIPT_DIR}/setup-igc.sh" "${DEST_DIR}" || exit 1
+"${SCRIPT_DIR}/setup-yq3.sh" "${DEST_DIR}" || exit 1
+"${SCRIPT_DIR}/setup-yq4.sh" "${DEST_DIR}" || exit 1

cca6c21095f5751e97d1e5356d9716a973b88a42
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2022-02-07 23:14:14 +0530

    updated rosa-cli
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -4,52 +4,52 @@ BIN_DIR=$(cat .bin_dir)
 
 ls -l "${BIN_DIR}"
 
-if [[ ! -f "${BIN_DIR}/jq" ]]; then
-  echo "jq not found"
-  exit 1
-fi
+# if [[ ! -f "${BIN_DIR}/jq" ]]; then
+#   echo "jq not found"
+#   exit 1
+# fi
 
-if [[ ! -f "${BIN_DIR}/yq3" ]]; then
-  echo "yq3 not found"
-  exit 1
-fi
+# if [[ ! -f "${BIN_DIR}/yq3" ]]; then
+#   echo "yq3 not found"
+#   exit 1
+# fi
 
-if [[ ! -f "${BIN_DIR}/yq4" ]]; then
-  echo "yq4 not found"
-  exit 1
-fi
+# if [[ ! -f "${BIN_DIR}/yq4" ]]; then
+#   echo "yq4 not found"
+#   exit 1
+# fi
 
-if [[ ! -f "${BIN_DIR}/igc" ]]; then
-  echo "igc not found"
-  exit 1
-fi
+# if [[ ! -f "${BIN_DIR}/igc" ]]; then
+#   echo "igc not found"
+#   exit 1
+# fi
 
-if [[ ! -f "${BIN_DIR}/helm" ]]; then
-  echo "helm not found"
-  exit 1
-fi
+# if [[ ! -f "${BIN_DIR}/helm" ]]; then
+#   echo "helm not found"
+#   exit 1
+# fi
 
-if [[ ! -f "${BIN_DIR}/argocd" ]]; then
-  echo "argocd not found"
-  exit 1
-fi
+# if [[ ! -f "${BIN_DIR}/argocd" ]]; then
+#   echo "argocd not found"
+#   exit 1
+# fi
 
-if ! "${BIN_DIR}/helm" version; then
-  echo "helm cli not configured properly"
-  exit 1
-fi
+# if ! "${BIN_DIR}/helm" version; then
+#   echo "helm cli not configured properly"
+#   exit 1
+# fi
 
 if ! "${BIN_DIR}/rosa" version; then
  echo "rosa cli not configured properly"
  exit 1
 fi
 
-if ! "${BIN_DIR}/gh" version; then
-  echo "gh cli not configured properly"
-  exit 1
-fi
+# if ! "${BIN_DIR}/gh" version; then
+#   echo "gh cli not configured properly"
+#   exit 1
+# fi
 
-if ! "${BIN_DIR}/glab" version; then
-  echo "glab cli not configured properly"
-  exit 1
-fi
+# if ! "${BIN_DIR}/glab" version; then
+#   echo "glab cli not configured properly"
+#   exit 1
+# fi

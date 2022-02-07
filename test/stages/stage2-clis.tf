8097d562d772ef0dc0c93039c9c235b85dbdd204
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2022-02-08 00:07:44 +0530

    updated clis script
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- test/stages/stage2-clis.tf
+++ test/stages/stage2-clis.tf
@@ -3,10 +3,10 @@ module "clis" {
 
   #clis = ["yq", "jq", "igc", "helm", "argocd", "rosa", "gh", "glab"]
   clis = ["rosa"]
-  
+
 }
 
-resource null_resource write_path {
+resource "null_resource" "write_path" {
   provisioner "local-exec" {
     command = "echo -n '${module.clis.bin_dir}' > .bin_dir"
   }

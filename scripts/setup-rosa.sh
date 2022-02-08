b839c8b5bc25b861b6a18c1fc67b8852dff4e94e
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2022-02-08 09:54:16 +0530

    updated rosa cli script?
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- scripts/setup-rosa.sh
+++ scripts/setup-rosa.sh
@@ -1,24 +1,23 @@
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
-
-DEST_DIR="$1"
+DEST_DIR="$1" 
 TYPE="$2"
-
 CLI_NAME="rosa"
 
+mkdir -p "${DEST_DIR}"
+BIN_DIR=$(cd "${DEST_DIR}"; pwd -P)
+
+#TGZ_PATH="${DEST_DIR}/rosa"
+#mkdir -p $TGZ_PATH
+
+
 FILENAME="rosa-linux.tar.gz"
-# TGZ_PATH="linux-amd64/rosa"
-  TGZ_PATH="${DEST_DIR}/rosa"
+# TGZ_PATH="linux-amd64/rosa"  
 if [[ "$TYPE" == "macos" ]]; then
   FILENAME="rosa-macosx.tar.gz"
   #TGZ_PATH="darwin-amd64/rosa"    
 fi
 
 CLI_URL="https://mirror.openshift.com/pub/openshift-v4/clients/rosa/latest/${FILENAME}"
-
-
-mkdir -p "${DEST_DIR}"
-
-BIN_DIR=$(cd "${DEST_DIR}"; pwd -P)
 
 if command -v "${BIN_DIR}/${CLI_NAME}" 1> /dev/null 2> /dev/null; then
   echo "${CLI_NAME} already installed in ${BIN_DIR}..."
@@ -32,6 +31,7 @@ if [[ -n "${COMMAND}" ]]; then
   ln -s "${COMMAND}" "${BIN_DIR}/${CLI_NAME}"
   COMMAND="${BIN_DIR}/${CLI_NAME}"
 else
+  echo "Current working dir : $PWD"
   TAR_FILE="${BIN_DIR}/${CLI_NAME}.tgz"
   echo "TAR_FILE is ${TAR_FILE}"
   if [[ -f "${TAR_FILE}" ]]; then
@@ -46,18 +46,18 @@ else
 
     curl -sLo "${TAR_FILE}" "${CLI_URL}"
     
-
     if [[ ! -f "${BIN_DIR}/${CLI_NAME}" ]]; then
       echo "${TAR_FILE} Installing..."
-     # tar xzf "${TAR_FILE}" "${TGZ_PATH}"
+      # tar xzf "${TAR_FILE}" "${TGZ_PATH}"
       tar xzf "${TAR_FILE}"
-      echo "${TAR_FILE} coping..."
-      chmod +x "${CLI_NAME}"
+      
+      echo " coping ${CLI_NAME} to ${BIN_DIR}..."
+      #chmod +x "${CLI_NAME}"
       cp "${CLI_NAME}" "${BIN_DIR}/${CLI_NAME}"
-      rm "${TGZ_PATH}"
+      # rm "${TGZ_PATH}"
     fi
 
-   rm "${TAR_FILE}"
+    rm "${TAR_FILE}"
     chmod +x "${BIN_DIR}/${CLI_NAME}"
   fi
 
--- test/stages/stage2-clis.tf
+++ test/stages/stage2-clis.tf
@@ -8,6 +8,7 @@ module "clis" {
 
 resource "null_resource" "write_path" {
   provisioner "local-exec" {
-    command = "echo -n '${module.clis.bin_dir}' > .bin_dir"
+    #command = "echo -n '${module.clis.bin_dir}' > .bin_dir"
+    command = "echo ${module.clis.bin_dir} > .bin_dir"
   }
 }

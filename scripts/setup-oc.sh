49077d8ab563049dbfd1456e898fdcad0bd3474b
Sean Sundberg <seansund@us.ibm.com>
2022-03-13 09:32:08 -0500

    Handle multiple clis from a single tar file
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/setup-oc.sh
+++ scripts/setup-oc.sh
@@ -12,18 +12,17 @@ fi
 
 URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-${FILETYPE}.tar.gz"
 
-CMD_NAME="oc"
+CMD_NAME="oc;kubectl"
 if [[ "${TYPE}" == "alpine" ]] && [[ ! -f /lib/libgcompat.so.0 ]]; then
-  CMD_NAME="oc-bin"
+  CMD_NAME="oc-bin;kubectl-bin"
 fi
 
-"${SCRIPT_DIR}/setup-binary-from-tgz.sh" "${DEST_DIR}" "${CMD_NAME}" "${URL}" oc
+"${SCRIPT_DIR}/setup-binaries-from-tgz.sh" "${DEST_DIR}" "${CMD_NAME}" "${URL}" "oc;kubectl"
 
 if [[ "${TYPE}" == "alpine" ]] && [[ ! -f /lib/libgcompat.so.0 ]]; then
-  echo "/lib/ld-musl-x86_64.so.1 --library-path /lib ${DEST_DIR}/oc-bin \$@" > ${DEST_DIR}/oc
+  echo "/lib/ld-musl-x86_64.so.1 --library-path /lib ${DEST_DIR}/oc-bin \$@" > "${DEST_DIR}/oc"
   chmod +x "${DEST_DIR}/oc"
-fi
 
-if [[ ! -e "${DEST_DIR}/kubectl" ]]; then
-  ln "${DEST_DIR}/oc" "${DEST_DIR}/kubectl" 1> /dev/null 2> /dev/null || echo -n ''
+  echo "/lib/ld-musl-x86_64.so.1 --library-path /lib ${DEST_DIR}/kubectl-bin \$@" > "${DEST_DIR}/kubectl"
+  chmod +x "${DEST_DIR}/kubectl"
 fi
